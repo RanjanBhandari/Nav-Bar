01b6e3285218893d14a76022c99c1d015805818c
RanjanBhandari <36957059+RanjanBhandari@users.noreply.github.com>
2018-04-26 23:50:19 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+  document.getElementsByClassName('side-nav')[0].style.width= '250px';
+  document.getElementsByClassName('main')[0].style.marginLeft= '250px';
+}
+function closeSlideMenu() {
+  document.getElementsByClassName('side-nav')[0].style.width= '0';
+  document.getElementsByClassName('main')[0].style.marginLeft= '0';
+}
